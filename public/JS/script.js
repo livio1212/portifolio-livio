3955153799ec267115752df9f302934b61066890
livio1212 <liviocosta229@gmail.com>
2025-04-30 18:46:43 -0300

    corrigindo response

--- public/JS/script.js
+++ public/JS/script.js
@@ -12,7 +12,7 @@ form.addEventListener('submit', async function (e) {
 
 
   try {
-    const response = await fetch('https://portfolio-backend-1e5b.onrender.com', {
+    const response = await fetch('https://portfolio-backend-1e5b.onrender.com/send-email', {
       method: 'post',
       headers: {
         'Content-Type': 'application/json'
