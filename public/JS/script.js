c4ef199487dc94a725de322d8b0be1e2ebf95dd1
livio1212 <liviocosta229@gmail.com>
2025-05-09 11:19:30 -0300

    trocando host do backend

--- public/JS/script.js
+++ public/JS/script.js
@@ -12,7 +12,7 @@ form.addEventListener('submit', async function (e) {
 
 
   try {
-    const response = await fetch('https://portfolio-backend-1e5b.onrender.com/send-email', {
+    const response = await fetch('https://portfolio-backend-occ7lw.fly.dev', {
       method: 'post',
       headers: {
         'Content-Type': 'application/json'
