f0026926cab4d1ca48a5dc26c977d83c183dfb0b
livio1212 <liviocosta229@gmail.com>
2025-05-09 11:30:15 -0300

    ajustando endpoits

--- public/JS/script.js
+++ public/JS/script.js
@@ -13,6 +13,7 @@ form.addEventListener('submit', async function (e) {
 
   try {
     const response = await fetch('https://portfolio-backend-occ7lw.fly.dev/send-email', {
+      
       method: 'post',
       headers: {
         'Content-Type': 'application/json'
